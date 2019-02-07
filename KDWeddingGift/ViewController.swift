3a4318c8fa6c9f4ea02e1f6b9e7606d9f7d82ba1
KoingDev <sea.saingkoing@gmail.com>
2019-02-07 21:28:12 +0700

    Project init

---
+++ KDWeddingGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KDWeddingGift
+//
+//  Created by KoingDev on 2/7/19.
+//  Copyright © 2019 KoingDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
